0f9d5e9e3a95c50a21e2b3e135d35e2934e21d56
Tatiana <bochkarevatanya1710@gmail.com>
2020-10-09 19:21:50 +0300

    login works

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from time import sleep
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from time import sleep
 
-browser = webdriver.Firefox(executable_path=r'E:\\Projects\\Instagram_bot\\geckodriver.exe')
-browser.get('https://wwww.instagram.com/')
+'''options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path=r'C:\geckodriver\geckodriver.exe')
+driver.get("http://www.instagram.com/")
+print ("Headless Firefox Initialized")
+driver.quit()''' # working start in case of fail
+
+browser = webdriver.Firefox(executable_path=r'C:\geckodriver\geckodriver.exe')
+browser.implicitly_wait(5) # устанавливаем пятисекундную задержку
+# Если Selenium не может найти элемент, он ждет, чтобы все загрузилось и пытается снова
+
+browser.get('https://www.instagram.com/')
+browser.implicitly_wait(5)
+# Следующие строки говорят боту найти ссылку с текстом Log in и кликнуть по ней.
+login_link = browser.find_element_by_name("username")
+
+print('found name')
+login_link.click()
+
 sleep(5)
-browser.close()
 
-# driver = webdriver.Firefox('geckodriver.exe')
+username_input = browser.find_element_by_css_selector("input[name='username']")
+password_input = browser.find_element_by_css_selector("input[name='password']")
 
+print('pressed input')
+
+username_input.send_keys("among_us.russia")
+password_input.send_keys("TrySomethingNew")
+
+print('inserted username/password')
+
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+
+print('hit login button')
+
+sleep(10)
+
+#browser.close()
