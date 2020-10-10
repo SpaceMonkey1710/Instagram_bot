a99e9b56267be82928056540a576d1fc68af51b8
Tatiana <bochkarevatanya1710@gmail.com>
2020-10-10 19:03:49 +0300

    instapy added

--- instapy.py
+++ instapy.py
@@ -2,6 +2,7 @@ from instapy import InstaPy # need to install library later
 
 session = InstaPy(username="among_us.russia", password="TrySomethingNew")
 session.login()
+
 session.like_by_tags(["amongus", "amongusmemes"], amount=5)
 session.set_dont_like(["naked", "nsfw"])
 session.set_do_follow(True, percentage=50)
