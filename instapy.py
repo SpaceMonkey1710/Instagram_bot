30af9c000b29f47b665561192721fd9dfbb4dbb2
Tatiana <bochkarevatanya1710@gmail.com>
2020-10-10 18:56:55 +0300

    added instapy

---
+++ instapy.py
@@ -0,0 +1,10 @@
+from instapy import InstaPy
+
+session = InstaPy(username="among_us.russia", password="TrySomethingNew")
+session.login()
+session.like_by_tags(["amongus", "amongusmemes"], amount=5)
+session.set_dont_like(["naked", "nsfw"])
+session.set_do_follow(True, percentage=50)
+session.set_do_comment(True, percentage=50)
+session.set_comments(["Nice!", "Lol!", ":heart_eyes:"])
+session.end()
